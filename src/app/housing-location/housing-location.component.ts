9b954d77c6ceb4eccc5c576c766278ada24235cb
lanvd <lanvd2000@gmail.com>
2023-07-12 10:17:57 +0700

    create housing-location component

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  templateUrl: './housing-location.component.html',
+  styleUrls: ['./housing-location.component.css']
+})
+export class HousingLocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

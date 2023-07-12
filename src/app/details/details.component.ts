7c54b7c4d8abd1551fcc865f776026fa9eef9e73
lanvd <lanvd2000@gmail.com>
2023-07-12 11:50:13 +0700

    add reactive form

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Housinglocation } from '../housinglocation';
 import { HousingService } from '../housing.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
@@ -10,8 +11,15 @@ import { HousingService } from '../housing.service';
 })
 export class DetailsComponent implements OnInit {
   housingLocation!: Housinglocation | undefined;
+  applyForm = this.fb.group({
+    firstName: '',
+    lastName: '',
+    email: ''
+  });
 
-  constructor(private route: ActivatedRoute, private service: HousingService) {
+  constructor(private route: ActivatedRoute,
+    private service: HousingService,
+    private fb: FormBuilder) {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.service.getHousingLocationById(housingLocationId);
   }
@@ -19,4 +27,11 @@ export class DetailsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  submitApplication() {
+    this.service.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
+  }
 }

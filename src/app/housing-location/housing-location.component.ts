0bd98315918561be988a658a5938270b7349639f
lanvd <lanvd2000@gmail.com>
2023-07-12 10:39:44 +0700

    test binding data

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent implements OnInit {
+  @Input() housingLocation!: Housinglocation;
 
   constructor() { }
 

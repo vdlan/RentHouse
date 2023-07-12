427877d364c3506aa381f07464a1b86d61471326
lanvd <lanvd2000@gmail.com>
2023-07-12 11:06:36 +0700

    add housing service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Housinglocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -7,110 +8,11 @@ import { Housinglocation } from '../housinglocation';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  housingLocationList: Housinglocation[] = [
-    {
-      id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: 'https://via.placeholder.com/600/92c952',
-      availableUnits: 4,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo: 'https://via.placeholder.com/600/771796',
-      availableUnits: 0,
-      wifi: false,
-      laundry: true
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo: 'https://via.placeholder.com/600/24f355',
-      availableUnits: 1,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 3,
-      name: 'Homesteady Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: 'https://via.placeholder.com/600/d32776',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 4,
-      name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
-      photo: 'https://via.placeholder.com/600/f66b97',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
-      photo: 'https://via.placeholder.com/600/56a8c2',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: 'https://via.placeholder.com/600/b0f7cc',
-      availableUnits: 5,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
-      photo: 'https://via.placeholder.com/600/54176f',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: 'https://via.placeholder.com/600/51aa97',
-      availableUnits: 10,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 9,
-      name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
-      photo: 'https://via.placeholder.com/600/810b14',
-      availableUnits: 6,
-      wifi: true,
-      laundry: true
-    }
-  ];
+  housingLocationList: Housinglocation[] = [];
 
-  constructor() { }
+  constructor(private housingService: HousingService) {
+    this.housingLocationList = housingService.getAllHousingLocations()
+   }
 
   ngOnInit(): void {
   }
---
+++ src/app/housing.service.ts
@@ -0,0 +1,120 @@
+import { Injectable } from '@angular/core';
+import { Housinglocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  housingLocationList: Housinglocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: 'https://via.placeholder.com/600/92c952',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: 'https://via.placeholder.com/600/771796',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: 'https://via.placeholder.com/600/24f355',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: 'https://via.placeholder.com/600/d32776',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: 'https://via.placeholder.com/600/f66b97',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: 'https://via.placeholder.com/600/56a8c2',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: 'https://via.placeholder.com/600/b0f7cc',
+      availableUnits: 5,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Oakland',
+      state: 'CA',
+      photo: 'https://via.placeholder.com/600/54176f',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 8,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: 'https://via.placeholder.com/600/51aa97',
+      availableUnits: 10,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 9,
+      name: 'Capital Safe Towns',
+      city: 'Portland',
+      state: 'OR',
+      photo: 'https://via.placeholder.com/600/810b14',
+      availableUnits: 6,
+      wifi: true,
+      laundry: true
+    }
+  ];
+
+  constructor() { }
+
+  getAllHousingLocations(): Housinglocation[] {
+    return this.housingLocationList;
+  }
+  
+  getHousingLocationById(id: number): Housinglocation | undefined {
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+}
